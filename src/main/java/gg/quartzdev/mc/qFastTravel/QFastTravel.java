9f1f29809e850a004c086032fe63ce59e978e5a2
QarthO <qartho@gmail.com>
2025-08-09 23:34:04 -0400

    super simplify

--- src/main/java/gg/quartzdev/mc/qFastTravel/QFastTravel.java
+++ src/main/java/gg/quartzdev/mc/qFastTravel/QFastTravel.java
@@ -2,13 +2,27 @@ package gg.quartzdev.mc.qFastTravel;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import dev.jorel.commandapi.CommandAPICommand;
+import io.papermc.paper.dialog.Dialog;
+import io.papermc.paper.registry.data.dialog.DialogBase;
+import io.papermc.paper.registry.data.dialog.type.DialogType;
+import net.kyori.adventure.text.Component;
+
 public final class QFastTravel extends JavaPlugin
 {
 
     @Override
     public void onEnable()
     {
-        FastTravelAPI.enable(this, 12345);
+        
+        CommandAPICommand fastTravel = new CommandAPICommand("qfasttravel")
+        .withAliases("fasttravel", "ft", "travel", "warp")
+        .withPermission("qfasttravel.use")
+        .executesPlayer((player, args) -> {
+            player.showDialog(fastTravelDialog());
+        });
+
+        fastTravel.register();
     }
 
     @Override
@@ -17,4 +31,11 @@ public final class QFastTravel extends JavaPlugin
         // Plugin shutdown logic
     }
 
+    public Dialog fastTravelDialog() {
+        Dialog dialog = Dialog.create(builder -> builder.empty()
+        .base(DialogBase.builder(Component.text("Test Title")).build())
+        .type(DialogType.notice()));
+
+        return dialog;
+    }
 }
